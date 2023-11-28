eaa7f410a0d3b002c8355199a70c75e44e5ccd71
buggedf11 <120758948+buggedf11@users.noreply.github.com>
2023-11-28 11:56:12 +0000

    Add files via upload

---
+++ fla.py
@@ -0,0 +1,13 @@
+from flask import render_template
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    from fla import Flask, render_template
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run()
