62758c3cda8f75d0cad1b3d0857b33847357bb8e
buggedf11 <120758948+buggedf11@users.noreply.github.com>
2023-12-01 13:58:09 +0000

    good job

--- fla.py
+++ fla.py
@@ -1,6 +1,7 @@
 from flask import render_template
 from flask import Flask
 import psutil
+import psutil
 
 app = Flask(__name__)
 
@@ -20,12 +21,16 @@ def weather():
 def date():
     return render_template('Date.html')
 
+
 @app.route('/other')
 def other():
     cpu_percent = psutil.cpu_percent()
     memory_percent = psutil.virtual_memory().percent
     disk_usage = psutil.disk_usage('/').percent
-    return render_template('other.html', cpu_percent=cpu_percent, memory_percent=memory_percent, disk_usage=disk_usage)
+    network_stats = psutil.net_io_counters()
+    network_bytes_sent = network_stats.bytes_sent
+    network_bytes_received = network_stats.bytes_recv
+    return render_template('other.html', cpu_percent=cpu_percent, memory_percent=memory_percent, disk_usage=disk_usage, network_bytes_sent=network_bytes_sent, network_bytes_received=network_bytes_received)
 
 if __name__ == '__main__':
     app.run(debug=False)
