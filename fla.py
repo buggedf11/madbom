cbea0843a5336296fca15ffe3a4ef55bf175eb5f
buggedf11 <120758948+buggedf11@users.noreply.github.com>
2025-01-11 20:59:55 +0000

    d

--- fla.py
+++ fla.py
@@ -1,7 +1,4 @@
-from flask import render_template
-from flask import Flask
-import psutil
-import psutil
+from flask import Flask, render_template, send_file
 
 app = Flask(__name__)
 
@@ -9,36 +6,13 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/sound.html')
-def sound():
-    return render_template('sound.html')
+@app.route('/loggedin')
+def loggedin():
+    return render_template('loggedin.html')
 
-
-@app.route('/clock')
-def clock():
-    return render_template('clock.html')
-
-@app.route('/weather')
-def weather():
-    return render_template('weather.html')
-
-@app.route('/date')
-def date():
-    return render_template('Date.html')
-
-@app.route('/dash.html')
-def dash():
-    return render_template('dash.html')
-
-@app.route('/other')
-def other():
-    cpu_percent = psutil.cpu_percent()
-    memory_percent = psutil.virtual_memory().percent
-    disk_usage = psutil.disk_usage('/').percent
-    network_stats = psutil.net_io_counters()
-    network_bytes_sent = network_stats.bytes_sent
-    network_bytes_received = network_stats.bytes_recv
-    return render_template('other.html', cpu_percent=cpu_percent, memory_percent=memory_percent, disk_usage=disk_usage, network_bytes_sent=network_bytes_sent, network_bytes_received=network_bytes_received)
+@app.route('/user.json')
+def user_json():
+    return send_file('user.json', mimetype='application/json')
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=True)
