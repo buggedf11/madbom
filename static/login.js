77e331cdc63af3fd4e58c05916aa7ab3fca4f277
buggedf11 <120758948+buggedf11@users.noreply.github.com>
2025-01-14 18:32:17 +0000

    fixing login for m user

--- static/login.js
+++ static/login.js
@@ -3,13 +3,15 @@ document.getElementById('loginForm').addEventListener('submit', async function(e
     const username = document.querySelector('input[name="username"]').value;
     const password = document.querySelector('input[name="password"]').value;
 
+    console.log('Entered Username:', username);
+    console.log('Entered Password:', password);
+
     const response = await fetch('/static/users.json');
     const users = await response.json();
 
-    const user = users.find(user => 
-        (user.username1 === username && user.password1 === password) || 
-        (user.username2 === username && user.password2 === password)
-    );
+    console.log('Fetched Users:', users);
+
+    const user = users.find(user => user.username === username && user.password === password);
 
     const messageDiv = document.getElementById('message');
     if (user) {
