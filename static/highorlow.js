7bfdfb031555de7606194e3369d11e38e1588a3f
buggedf11 <120758948+buggedf11@users.noreply.github.com>
2025-01-15 13:13:31 +0000

    changes to layout of h and L

--- static/highorlow.js
+++ static/highorlow.js
@@ -5,7 +5,7 @@ let currentCard = null;
 let previousCard = null;
 
 const suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades'];
-const values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+const values = ['2', '3', '4', '5', '6', '7', '8', '9', 'J', 'Q', 'K', 'A'];
 
 async function fetchUsers() {
     try {
